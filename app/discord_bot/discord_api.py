196f9c1617e307ae4d352073375b640d4f1031f9
Nikhil Panchal <37629345+nikhilpanchal1@users.noreply.github.com>
2023-05-22 15:47:58 -0600

    Temp to 1.3, finished basic implementation with davinci 003
    Todo: Make gpt-3-turbo work later

--- app/discord_bot/discord_api.py
+++ app/discord_bot/discord_api.py
@@ -1,12 +1,11 @@
 from dotenv import load_dotenv
 import os
 import discord
-import openai
+from app.chatgpt_ai.openai import chatgpt_response
 
 load_dotenv()
 
 discord_token = os.getenv('DISCORD_TOKEN')
-openai_api_key = os.getenv('CHATGPT_APIKEY')
 
 class MyClient(discord.Client):
     async def on_ready(self):
@@ -18,43 +17,16 @@ class MyClient(discord.Client):
             return
         command, user_input = None, None
     
-        for text in ['/gpt', '/gpt4', 'gpt3']:
+        for text in ['/gpt', 'gpt3']:
             if message.content.startswith(text):
                 command = message.content.split(' ')[0]
                 user_input = message.content.replace(text, '')
                 print(command, user_input)
                 
-        if command == '/gpt' or command == '/gpt4' or command == '/gpt3':
+        if command == '/gpt' or command == '/gpt3':
             bot_response = chatgpt_response(prompt = user_input)
             await message.channel.send(f"Answer:{bot_response}")
 
 intents = discord.Intents.default()
 intents.message_content = True
 client = MyClient(intents=intents)
-
-
-# client = discord.Client()
-# openai.api_key = 'YOUR_OPENAI_API_KEY_HERE '
-
-# conversation_history = []
-
-# @client.event
-# async def on_message(message):
-#     if message.author == client.user:
-#         return
-
-#     if message.content.startswith('/gpt'):
-#         text = message.content[5:]
-#         conversation_history.append({"role": "user", "content": text})
-#         response = openai.ChatCompletion.create(
-#             model="gpt-4.0-turbo",
-#             messages=conversation_history
-#         )
-#         conversation_history.append({"role": "assistant", "content": response['choices'][0]['message']['content']})
-#         await message.channel.send(response['choices'][0]['message']['content'])
-
-#     elif message.content.startswith('/gpt clean'):
-#         conversation_history.clear()
-
-# client.run('YOUR_DISCORD_BOT_TOKEN_HERE ')
-
